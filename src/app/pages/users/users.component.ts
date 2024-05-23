bdcc51a2640ebfe4af1bdf49371028da6bab371d
fernandodataintegra <146885068+fernandodataintegra@users.noreply.github.com>
2024-05-23 15:09:26 -0300

    Melhorias

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../_models/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
 
  // users: String[] = ['Fernando', 'Ravih', 'Eloáh', 'Neinha'];
+
+ userSeleciona: User | undefined;
+ userForm: FormGroup = new FormGroup({});
+
+constructor(private fb : FormBuilder){}
+  ngOnInit(): void {
+    this.initilizeForm();
+  }
+
+initilizeForm(){
+  this.userForm = this.fb.group({
+    nome:['',[Validators.required, Validators.maxLength(50)]],
+    idade:['',[Validators.required, Validators.min(12), Validators.max(100)]],
+
+  });
+}
+
+SubmitFrom(){
+  if(this.userForm.valid){
+   this.users.push(this.userForm.value);
+   this.userForm.reset();
+  }
+}
+
  users: User[] = [
   {
     nome: 'Fernando Abreu',
@@ -28,6 +53,6 @@ export class UsersComponent {
   }
  ];
  infoUserSelecionado(user: User){
-  console.log(user);
+this.userSeleciona = user;
  }
 }
